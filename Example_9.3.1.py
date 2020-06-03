f4156c120facb52e3a4a622aa76316f94aa133ba
Leung Wing Kwan <liangyj@email.com>
2020-06-03 14:02:10 +0800

    Add files via upload

--- Example_9.3.1.py
+++ Example_9.3.1.py
@@ -3,5 +3,6 @@ import matplotlib.pyplot as plt
 x = [100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
 y = [45, 51, 54, 61, 66, 70, 74, 78, 85, 89]
 
-plt.plot(x, y,linewidth=2)
+plt.plot(x, y, "ro")
+plt.plot(x, y)
 plt.show()
