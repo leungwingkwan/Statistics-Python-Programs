85cbbd8a069a3591251d0b62eecb1f8e57cbdc10
Leung Wing Kwan <liangyj@email.com>
2020-07-20 17:19:47 +0800

    Add files via upload

--- Example_13.3.1.py
+++ Example_13.3.1.py
@@ -35,36 +35,81 @@ def determine_m(P):
        return loop_i, tmp_p
     else:
        return None, None
+def rsmat(arbmat):
+    """ Convert an arbitrary matrix to a simplest matrix """
+    arbmat = arbmat.astype(float)
+    row_number, column_number = arbmat.shape
+    if row_number == 1:
+        if arbmat[0, 0] != 0:
+            return (arbmat/arbmat[0, 0])
+        else:
+            return arbmat
+    else:
+        rc_number = min(row_number, column_number)
+        anarbmat = arbmat.copy()
+        r = 0
+        for n in range(rc_number):
+            s_row = -1
+            for i in arbmat[r:row_number, n]:
+                s_row += 1
+                if abs(i) > 1e-10:
+                    anarbmat[r, :] = arbmat[s_row+r, :]
+                    for j in range(r, row_number):
+                        if j < s_row+r:
+                            anarbmat[j+1, :] = arbmat[j, :]
+                    arbmat = anarbmat.copy()
+            if abs(anarbmat[r, n]) > 1e-10:
+                anarbmat[r, :] = anarbmat[r, :] / anarbmat[r, n]
+                for i in range(row_number):
+                    if i != r:
+                        anarbmat[i, :] -= \
+                        anarbmat[i, n]*anarbmat[r, :]
+            arbmat = anarbmat.copy()
+            if abs(arbmat[r, n]) < 1e-10:
+                r = r
+            else:
+                r = r + 1
+        for m in range(column_number):
+            if abs(arbmat[-1, m]) > 1e-10:
+                arbmat[-1, :] = arbmat[-1, :]/arbmat[-1, m]
+                for i in range(row_number-1):
+                    arbmat[i, :] -= \
+                    arbmat[i, m]*arbmat[-1, :]
+                break
+            
+        return arbmat
 
 ####################
 # Step 1: 遍历性
 ####################
 p = np.array(data_1)
-print('Data 1=========================')
+print('=======================Step 1=======================')
 m, p_m = determine_m(p)
 print('m =', m)
-print('p ^', str(m), '=', p_m)
+print('p ^', str(m), '=')
+print(p_m)
 
 ###################
 # Step 2:
 ###################
-p_rank = np.linalg.matrix_rank(p)
-print('p rank = ', p_rank)
+print('=======================Step 2=======================')
 print('p = ')
 print(p)
+p_rank = np.linalg.matrix_rank(p)
 #
 # use first 4 lines instead
 #
 p_t = np.transpose(p)
 print('p_t = ')
 print(p_t)
+
 p_1 = np.eye(len(p))
 print('p_1 = ')
 print(p_1)
 p_x = p_t - p_1
 print('p_x = ')
 print(p_x)
-p_x_a = p_x[0:p_rank - 1]
+p_x_a = p_x[0:4]
 p_x_a = np.vstack((p_x_a, np.ones(len(p))))
 print('p_x_a = ')
 print(p_x_a)
